ae9f24975aabf8bbe728fefc7d7ce58de7572027
Arnav2407 <bhatnagararnav61@gmail.com>
2026-06-05 19:09:41 +0530

    second

---
+++ JavaScript Course/Variables.js
@@ -0,0 +1,17 @@
+let variable1 = 3;
+console.log(2);
+
+let calculation = 2 + 2;
+console.log(calculation);
+console.log(calculation + 2);
+
+let result = calculation + 2;
+console.log(result);
+
+let message  = 'hello';
+console.log(message);
+
+const variable2 = 5;
+var variable3 = 6;
+
+console.log(typeof variable1);
